1dd903c6cce4c22fe3ddbffda29d776da160a7db
maktechdev <mub541@hotmail.com>
2022-07-04 17:28:50 +0200

    Adds backgrounds and links scripts to index

---
+++ scripts/car.js
@@ -0,0 +1,14 @@
+class Car {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        console.log("a new car made")
+    }
+
+    draw() {
+        console.log("draw car")
+    }
+
+}
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementById("myCanvas");
+console.log(canvas);
+canvas.height = window.innerHeight;    
+canvas.width = 200; 
