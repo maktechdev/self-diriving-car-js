87263a44e7c41ca2eaa11c8d82aa0ed2ef976ce2
maktechdev <39600404+maktechdev@users.noreply.github.com>
2022-07-06 00:38:13 +0200

    Modifies car class

--- scripts/car.js
+++ scripts/car.js
@@ -6,25 +6,34 @@ class Car {
         this.height = height;
         this.controls = new Controls();
         this.speed = 0;
-        this.maxSpeed = 15;
+        this.angle = 0;
+        this.maxSpeed = 5;
         this.acceleration = 0.03;
 
 
     }
 
     update() {
-        if (this.controls.forward) {
+        if (this.controls.forward) 
             this.speed += this.acceleration;
-            this.y -= this.speed;
-            if (this.speed >= this.maxSpeed) {
+                      
+        if (this.controls.reverse)
+            this.speed -= this.acceleration;
+        
+        if (this.speed > this.maxSpeed) {
                 this.speed = this.maxSpeed;
             }
-            console.table(this.speed);
+        if (this.speed < -this.maxSpeed / 2) {
+                this.speed = -this.maxSpeed / 2;
+            }
+
+
+        if (this.maxSpeed > 100) {
+            this.x += Math.cos(this.angle) * this.speed;
+            this.y += Math.sin(this.angle) * this.speed; 
         }
-        if (this.controls.reverse) {
-            this.y += 1;
-            console.table(this.speed);
-        }
+            
+        this.y -= this.speed;
 
         
     }
--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,6 @@ const speedDisplay = document.getElementById("speedDisplay");
 
 canvas.width = 200; 
 
-
-
 const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx)
