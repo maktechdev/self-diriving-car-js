7c113469f756101f3e868ed41955f337c09079f4
maktechdev <39600404+maktechdev@users.noreply.github.com>
2022-07-06 02:53:44 +0200

    Implements turning for car

--- scripts/car.js
+++ scripts/car.js
@@ -9,43 +9,47 @@ class Car {
         this.angle = 0;
         this.maxSpeed = 5;
         this.acceleration = 0.03;
-
-
     }
 
     update() {
         if (this.controls.forward) 
             this.speed += this.acceleration;
-                      
+        
         if (this.controls.reverse)
             this.speed -= this.acceleration;
         
-        if (this.speed > this.maxSpeed) {
-                this.speed = this.maxSpeed;
-            }
-        if (this.speed < -this.maxSpeed / 2) {
-                this.speed = -this.maxSpeed / 2;
-            }
+        if (this.speed > this.maxSpeed) 
+            this.speed = this.maxSpeed;
+        
+        if (this.speed < -this.maxSpeed / 2)
+            this.speed = -this.maxSpeed / 2;
+        
+        if (this.controls.left) {
+            this.angle += .03;
+        }
 
-
-        if (this.maxSpeed > 100) {
-            this.x += Math.cos(this.angle) * this.speed;
-            this.y += Math.sin(this.angle) * this.speed; 
+        if (this.controls.right) {
+            this.angle -= .03;
         }
             
-        this.y -= this.speed;
-
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed; 
         
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+        ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            -this.width / 2,
+            -this.height / 2,
             this.width,
             this.height
         );
         ctx.fill();
+        ctx.restore();
     }
 
 }
